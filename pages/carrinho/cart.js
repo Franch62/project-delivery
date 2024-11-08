1dad7788c6f665e205d137641118c228dde91c44
Francis <28francis.junior@gmail.com>
2024-11-08 20:44:36 -0300

    host

--- pages/carrinho/cart.js
+++ pages/carrinho/cart.js
@@ -8,7 +8,7 @@ function fetchCartItems() {
   cartItems.forEach((item) => {
     const itemTotal = item.price * item.quantity;
     total += itemTotal;
-    const srcImage = `http://localhost:3000/api/${item.image_url}`.replace(
+    const srcImage = `https://api-order-menu.onrender.com/api/${item.image_url}`.replace(
       "\\",
       "/"
     );
@@ -33,19 +33,15 @@ function fetchCartItems() {
   document.getElementById("cart-total").innerText = `R$ ${total.toFixed(2)}`;
 }
 
-// Função para remover um item do carrinho
 function removeFromCart(itemId) {
   let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
   cartItems = cartItems.filter((item) => item._id !== itemId);
   localStorage.setItem("cartItems", JSON.stringify(cartItems));
-  fetchCartItems(); // Recarrega os itens após a remoção
+  fetchCartItems();
 }
 
-// Função para finalizar a compra
 function finalizePurchase() {
   alert("Compra finalizada! Obrigado pela preferência.");
-  // Aqui você pode redirecionar o usuário, limpar o carrinho ou enviar um pedido ao backend.
 }
 
-// Carrega os itens do carrinho quando a página é carregada
 window.onload = fetchCartItems;
